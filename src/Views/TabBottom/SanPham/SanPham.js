791a92f31ae5fa2e374318538bfa42c335fabae8
stylishjoker <animetplink@gmail.com>
2022-11-19 12:46:12 +0700

    !

--- src/Views/TabBottom/SanPham/SanPham.js
+++ src/Views/TabBottom/SanPham/SanPham.js
@@ -1,12 +1,20 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { View, Text, StyleSheet, ScrollView, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  SafeAreaView,
+  TextInput,
+} from "react-native";
 import { SelectList } from "react-native-dropdown-select-list";
 
 import {
   SCREEN_HEIGHT,
   STATUS_BAR_HEIGHT,
   WINDOW_HEIGHT,
+  SCREEN_WiDTH,
 } from "../../../App/ScreenDefault";
 import ElementSP from "../ElementSP";
 import BASE_URL from "../../../Api/config";
@@ -15,7 +23,6 @@ import Header from "../components/Header";
 const SanPham = () => {
   const [products, setProducts] = useState([]);
   const [selected, setSelected] = useState("Tất cả");
-  console.log(selected);
   const data = [
     {
       key: 1,
@@ -45,7 +52,8 @@ const SanPham = () => {
       <Header name="List sản phẩm" />
       <View style={styles.option}>
         <SelectList
-          dropdownStyles={{ backgroundColor: "white" }}
+          dropdownStyles={styles.dropdownStyles}
+          search={false}
           boxStyles={styles.SelectList}
           placeholder={selected}
           maxHeight={100}
@@ -53,21 +61,24 @@ const SanPham = () => {
           data={data}
           save="value"
         />
+        <TextInput style={styles.TextInput} placeholder="Nhập tên sản phẩm" />
       </View>
-      <ScrollView style={{ height: SCREEN_HEIGHT }}>
-        <View style={styles.container}>
-          {products.map((_element) => {
-            return (
-              <ElementSP
-                key={_element.id}
-                name={_element.name}
-                price={_element.price}
-                url={_element.imgLink}
-              />
-            );
-          })}
-        </View>
-      </ScrollView>
+      <View style={{ height: WINDOW_HEIGHT - 190, width: SCREEN_WiDTH }}>
+        <ScrollView style={styles.ScrollView} pagingEnabled>
+          <View style={styles.container}>
+            {products.map((_element) => {
+              return (
+                <ElementSP
+                  key={_element.id}
+                  name={_element.name}
+                  price={_element.price}
+                  url={_element.imgLink}
+                />
+              );
+            })}
+          </View>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   );
 };
@@ -86,13 +97,35 @@ const styles = StyleSheet.create({
     fontWeight: "600",
   },
   option: {
+    padding: 10,
     height: 60,
     width: "100%",
     zIndex: 100,
+    display: "flex",
+    flexDirection: "row",
+    backgroundColor: "white",
+    marginTop: 5,
   },
   SelectList: {
-    marginTop: 5,
     backgroundColor: "white",
+    height: 40,
+  },
+  dropdownStyles: {
+    height: 300,
+    backgroundColor: "white",
+  },
+  TextInput: {
+    flex: 1,
+    borderColor: "#999",
+    borderWidth: 1,
+    height: 40,
+    alignSelf: "center",
+    borderRadius: 10,
+    marginLeft: 10,
+    padding: 5,
+  },
+  ScrollView: {
+    height: WINDOW_HEIGHT,
   },
 });
 
